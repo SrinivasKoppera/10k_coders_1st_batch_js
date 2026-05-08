4d3463478288d12eea7dae8a648e9a19ee167330
ravikumargora <rkg@impact.dk>
2026-05-08 16:29:21 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+const name = "John";
+let age = 30;
+var city = "New York";
+console.log(`Hello, ${name}!`);
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", function () {
+  alert("Hello, World!");
+});
